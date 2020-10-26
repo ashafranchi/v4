1187b4319abe9391a57841f2368081acc61a01f4
Asha <asha17@brown.edu>
2020-10-25 22:32:30 -0700

    updating projects - 1

---
+++ content/featured/ExpenseTracker/index.md
@@ -0,0 +1,13 @@
+---
+date: '3'
+title: 'ExpenseTracker'
+cover: './octoprofile.png'
+github: 'https://github.com/bchiang7/octoprofile'
+external: 'https://expense-tracker-5im27hmsl.vercel.app/'
+tech:
+  - React.js
+  - Context API
+showInProjects: true
+---
+
+I used React.js to build a sleek, easy-to-use expense tracker. I used hooks including useState, useContext, and useReducer, to make the app fully functional. It takes in new transactions, records a history, and allows for deleting items.
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+// import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Icon } from '@components/icons';
 import { socialMedia } from '@config';
@@ -67,29 +67,29 @@ const StyledCredit = styled.div`
   }
 `;
 
-const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
+const Footer = () => 
+// const [githubInfo, setGitHubInfo] = useState({
+//   stars: null,
+//   forks: null,
+// });
 
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      return;
-    }
-    fetch('https://api.github.com/repos/bchiang7/v4')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
-  }, []);
+// useEffect(() => {
+//   if (process.env.NODE_ENV !== 'production') {
+//     return;
+//   }
+//   fetch('https://api.github.com/repos/bchiang7/v4')
+//     .then(response => response.json())
+//     .then(json => {
+//       const { stargazers_count, forks_count } = json;
+//       setGitHubInfo({
+//         stars: stargazers_count,
+//         forks: forks_count,
+//       });
+//     })
+//     .catch(e => console.error(e));
+// }, []);
 
-  return (
+  (
     <StyledFooter>
       <StyledSocialLinks>
         <ul>
@@ -108,7 +108,7 @@ const Footer = () => {
         <a href="https://brittanychiang.com/">
           <div>Asha Franchi 2020. Inspired by Brittany Chiang.</div>
 
-          {githubInfo.stars && githubInfo.forks && (
+          {/* {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
               <span>
                 <Icon name="Star" />
@@ -119,15 +119,15 @@ const Footer = () => {
                 <span>{githubInfo.forks.toLocaleString()}</span>
               </span>
             </div>
-          )}
+          )} */}
         </a>
       </StyledCredit>
     </StyledFooter>
-  );
-};
+  )
+;
 
-Footer.propTypes = {
-  githubInfo: PropTypes.object,
-};
+// Footer.propTypes = {
+//   githubInfo: PropTypes.object,
+// };
 
 export default Footer;
